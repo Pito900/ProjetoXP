7a677abb320c87d7e34e13d0829390c068892ac4
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-07-16 14:55:43 -0300

    feat: fiz o api.js e o services.js e dockerizei toda a aplicação e a base de dados

--- src/DataBase/config/config.js
+++ src/DataBase/config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+
+modelue.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "ProjetoXp",
+    "host": process.env.DB_HOSTNAME || "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {

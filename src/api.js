f75b23a16f0dd46796cc6273a108946a202d5811
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-07-17 17:47:51 -0300

    feat: fiz o middleware de erro e a estrutura báse das rotas

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
+app.use(require('./routes'));
+app.use(require('./middlewares/error'));
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+require('express-async-errors');
+
+const routers = express.Router();
+
+module.exports = routers; 

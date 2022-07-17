851fd94813f4fd0dd7db0e38d5e36e6cbf3a29b2
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-07-17 11:59:30 -0300

    build: configurei o eslint no projeto

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;

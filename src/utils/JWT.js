906f49f289226dae0b669e8d6d4ae4abe458ca0c
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-07-21 19:01:29 -0300

    feat: fiz o swagger do Get /assets/ativos/:codAtivo

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -13,10 +13,13 @@ const tokenVerification = (token) => {
     if (!token) { // quando o token é vazio
         return 'Token not found';
     }
-    try { // se o token vier correto 
-        const [, tk] = token.split(' '); // para o swagger...no inmnia é só usar o token padrão
-
+    try { // se o token vier correto
+        const [a, tk] = token.split(' '); // pensei nesse if para rodar no insomnia e no swagger sem precisar eu ficar mudando sempre a variável
+        if (a === 'Bearer') {
         const tokenValidationResult = jwt.verify(tk, JWT_SECRET, jwtConfig);
+        return tokenValidationResult;
+        }
+        const tokenValidationResult = jwt.verify(token, JWT_SECRET, jwtConfig);
         return tokenValidationResult;
     } catch (e) { // o token veio, porém é inválido ou ta expirado (pasou 10 d)
         return 'Expired or invalid token';

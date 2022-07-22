4480eb88c976326f5a5c45bae86f2093d95aabda
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-07-22 12:33:35 -0300

    Feat: teste para a rota GET /assets/ativos/:codAtivo (controller) para quando há o ativo na base de dados

--- src/test/unit/ativosController.test.js
+++ src/test/unit/ativosController.test.js
@@ -3,10 +3,9 @@ const sinon = require('sinon');
 const { gettingAtivoByCodAtivoController } = require('../../controllers/ativosController');
 
 describe('Testando o controller do endpoint GET /assets/ativos/:codAtivo', () => {
+    const res = {};
+    const req = {};
     describe(('Quando não há o ativo na base de dados'), () => {
-        const res = {};
-        const req = {};
-    
         before(() => {
             req.params = {
                 codAtivo: 65478916,
@@ -22,8 +21,33 @@ describe('Testando o controller do endpoint GET /assets/ativos/:codAtivo', () =>
 
         });
         it('Deve retornar uma mensagem "Ativo não encontrado."', async () => {
-            const result = await gettingAtivoByCodAtivoController(req, res);
+            await gettingAtivoByCodAtivoController(req, res);
             expect(res.json.calledWith({ message: 'Ativo não encontrado.' })).to.be.equal(true)
+        });
+    });
+    describe(('Quando há o ativo na base de dados'), () => {
+        before(() => {
+            req.params = {
+                codAtivo: 1,
+              };
+            res.status = sinon.stub()
+              .returns(res);
+            res.json = sinon.stub()
+              .returns();
+        })
+        it('O status code deve retornar 200', async () => {
+            await gettingAtivoByCodAtivoController(req, res);
+            expect(res.status.calledWith(200)).to.be.equal(true)
+
+        });
+        it('Deve retornar um objeto, do ativo especificado, com dados corretos.', async () => {
+            await gettingAtivoByCodAtivoController(req, res);
+            expect(res.json.calledWith({
+                "codAtivo": 1,
+                "ticker": "PETR4",
+                "qtdDisponivel": 320,
+                "valor": 250
+            })).to.be.equal(true)
         });
     });
 });
